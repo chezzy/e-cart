aa3cf8073b7e585cc8f0ecea18e3da8ec0c19b5f
Dizzy <ches@ukr.net>
2016-05-19 22:15:48 +0300

    + Класс структура для одного товара

---
+++ App/CartItem.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+/**
+ * Классс структура товара
+ *
+ * Class CartItem
+ * @package App
+ */
+class CartItem
+{
+    private $id;
+    private $count;
+    private $price;
+
+    public function __construct($id, $count, $price)
+    {
+        $this->id = $id;
+        $this->count = $count;
+        $this->price = $price;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getCount()
+    {
+        return $this->count;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function getCost()
+    {
+        return $this->price * $this->count;
+    }
+}
